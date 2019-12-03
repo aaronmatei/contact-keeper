b43289527d2257a31f91ba61a87252e2807cd5c0
aaronmate <aronique@gmail.com>
2019-12-03 12:02:28 +0300

    prepare to deploy

--- .history/server_20191203115200.js
+++ .history/server_20191203115200.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const connectDB = require('./config/db');
-const path = require('path');
 const app = express();
 
 // connect to DB
@@ -22,7 +21,7 @@ app.use('/api/contacts', require('./routes/contacts'));
 if (process.env.NODE_ENV === 'production') {
 	// Set static folder
 	app.use(express.static('client/build'));
-	app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+	app.get('*');
 }
 
 const PORT = process.env.PORT || 5000;
